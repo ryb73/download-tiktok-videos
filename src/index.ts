5cea616f259986060e23a3f8c623faeaed4a9267
Ryan <rpbiwer@gmail.com>
2025-01-13 11:50:10 -0500

    Start shit that I might scrap

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello from TypeScript");
+import { array, strict, string } from "io-ts";
+
+const schema = strict({
+  Activity: strict({
+    "Favorite Videos": strict({
+      FavoriteVideoList: array(
+        strict({
+          Link: string,
+        })
+      ),
+    }),
+  }),
+});
